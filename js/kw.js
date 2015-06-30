a52aa071768546602c8c98cd2eb7a02954772c33
Nicole Harris <n.harris@kabucreative.com.au>
2015-06-30 12:09:33 +0100

    Add fastclick for mobile touch events

--- js/kw.js
+++ js/kw.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+    // Activate fastclick
+    $(function() {
+        FastClick.attach(document.body);
+    });
+
     // FAQ accordions and tabs
 
     $('.faqs').accordion({
